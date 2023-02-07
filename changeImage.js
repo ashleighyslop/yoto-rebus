3c8d68ea2ae673a400e7f95f967aef949597481d
Ashleigh Hyslop <ashleighhyslop@Ashleighs-MacBook-Pro.local>
2023-02-07 18:04:27 +0000

    initalise repo

---
+++ changeImage.js
@@ -0,0 +1,36 @@
+var i = 0;
+
+// Crappy POC code
+
+function prev() {
+  i = Math.max(i - 1, 0);
+  document.querySelector(".pixelart").src = images[i];
+}
+
+function next() {
+  i = Math.min(i + 1, images.length - 1);
+  document.querySelector(".pixelart").src = images[i];
+}
+
+var imgYoto =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/0CYPYWVHYLURB3_wYr0tx0bUKYb4mgXTjn_8cx89ciU";
+
+var imgNight =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/7buOLfEt-h1-axsTKzPvDAlLK6oi9U_4N0CUsmB0RYY";
+
+var imgBee =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/Jc3p_I7ZHVjzDdkrakWZAvqPKPzJJah7ZiLzgcGKCsQ";
+
+var imgFour =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/ceJFo6GR12is75P3dyESEnEq5Tqo_6MxpE96FDeMsq0";
+
+var imgSanta =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/f8GZbRwTQPxfjTZ8b4yUUR7Tkqdb2xM7u0nXkpNUtEw";
+
+var imgQuestion =
+  "https://yoto-card-api-prod-media.s3.eu-west-2.amazonaws.com/yoto/UAQO5kbImNTPRcr2yywpPlR_MmzZtVsmhRUg6t5a_9s";
+
+var images = [imgYoto, imgNight, imgBee, imgFour, imgSanta, imgQuestion];
+
+// Init
+document.querySelector(".pixelart").src = images[0];
